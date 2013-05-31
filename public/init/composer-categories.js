59fb27fc85a5707c086f3914e12daa01f57e6178
gerardobort <gerardobort@gmail.com>
2013-05-31 01:17:38 -0300

    timeline tweaks

--- public/init/composer-categories.js
+++ public/init/composer-categories.js
@@ -6,7 +6,9 @@
         height:     '600',
         source:     '/api/composer-category-timeline/' + $('[data-category-uri]').data('category-uri'),
         embed_id:   'composer-category-timeline',
-        font: 'Pacifico-Arimo'
+        font: 'Pacifico-Arimo',
+        hash_bookmark: true, 
+        js: '/library/custom-timeline-min.js'
     });
 
 }(jQuery));
